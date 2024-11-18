f68549d90005ff56c45e8ca71dbe6b6256083c6a
Aynalem18 <aynalemfisum@gmail.com>
2024-11-18 10:27:02 -0500

    first-commit

---
+++ netflix-clone/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+// import Home from './Pages/Home/Home'
+
+
+function App() {
+ 
+
+  return (
+    
+    <div className='App'>
+      <h1>Hello</h1>
+
+      {/* <Home/> */}
+
+    </div>
+  )
+}
+
+export default App
